406a5385a2d972c383f141e0133d7631071a9a1d
JHankins09 <Hankins.JamesB@gmail.com>
2019-07-23 09:05:07 -0400

    Update to deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
-const userEvents = require('./user/events')
+// const userEvents = require('./user/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
