f0271210d9f82c566c9f236231f7632db7480eb2
JHankins09 <Hankins.JamesB@gmail.com>
2019-07-23 08:55:37 -0400

    Commit for Deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,15 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const userEvents = require('./user/events')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+// your JS code goes here
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', userEvents.onSignUp)
+  $('#sign-in').on('submit', userEvents.onSignIn)
+  $('#change-pw').on('submit', userEvents.onChangePassword)
+  $('#sign-out').on('submit', userEvents.onSignOut)
 })
